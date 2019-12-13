e8c5b853be4187aef2eaf97342f04b5c7bf17508
srivatsa <svatsa159@gmail.com>
2019-12-13 12:47:49 +0530

    SW Unreg

--- static/index/index.js
+++ static/index/index.js
@@ -31,3 +31,7 @@ function saveAndRedirect(){
     );
     
 }
+navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+     registration.unregister()
+   } })
